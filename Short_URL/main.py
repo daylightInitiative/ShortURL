74b70981856adbe79139517e5bf4c85d02b503ff
daylightInitiative <202697339+daylightInitiative@users.noreply.github.com>
2025-06-02 14:03:46 -0400

    converted to use SQLite database, added copy to clipboard button, general improvements

--- Short_URL/main.py
+++ Short_URL/main.py
@@ -2,12 +2,20 @@ from flask import Flask, request, redirect, jsonify, render_template, send_from_
 from hashlib import sha256
 from flask_cors import CORS
 from urllib.parse import urlparse
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
-CORS(app) # enable CORS
 
-url_dict = {}
-url_counter = 1
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'urls.db')}" # cross-platform usage of os.path.join
+CORS(app) # enable CORS
+db = SQLAlchemy(app)
+
+class URL(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    URLcode = db.Column(db.String(2096), unique=True, nullable=False)  # Max URL length
+    original_url = db.Column(db.String(2096), nullable=False)
 
 def is_valid_url(url):
     parsed = urlparse(url)
@@ -17,7 +25,6 @@ def is_valid_url(url):
 def index():
     return render_template('index.html')
 
-# thinking about 
 def get_shortened_url(long_url) -> str:
     # utf-8 is the standard encoding for the web
     combined_str = long_url + "ShortURLRandom" # our salt
@@ -26,22 +33,22 @@ def get_shortened_url(long_url) -> str:
 
     # this way its (almost) completely random
     # craft the url from this hash
-    new_url = f"http://localhost:5000/{url_code}"
-    return new_url, url_code
+    new_short_url = f"http://localhost:5000/{url_code}"
+    return new_short_url, url_code
+
+# make sure this is here because browsers will ask for this every time a webpage is loaded
+@app.route('/favicon.ico')
+def favicon():
+    return '', 204  
 
 @app.route('/<short_code>', methods=['GET'])
 def redirect_to_long_url(short_code):
     # Look up the long URL using the short code
-    long_url = url_dict.get(short_code)
-    
-    if long_url:
-        return redirect(long_url)
-    else:
-        abort(404)
+    short_url = URL.query.filter_by(URLcode=short_code).first_or_404()     #.first_or_404()
+    return redirect(short_url.original_url)
 
 @app.route('/shorten', methods=['POST'])
 def serve_shortened_url():
-    global url_dict
     data = request.get_json()
 
     long_url = data.get('longURL')
@@ -55,26 +62,32 @@ def serve_shortened_url():
     small_url, small_code = get_shortened_url(long_url)
 
     # check if it already exists then we just redirect
-    if url_dict.get(small_code):
-        return jsonify({"shortURL": small_url}), 200
+    url_already_exists = URL.query.filter_by(URLcode=small_code).first()
+    if url_already_exists:
+       return jsonify({"shortURL": small_url}), 200
 
-    url_dict[small_code] = long_url
+    new_url = URL(URLcode=small_code, original_url=long_url)
+    db.session.add(new_url)
+    db.session.commit()
 
-    print(f"new URL: {small_url}")
+    print(f"new URL: {small_url}({long_url})")
     output = {
         "shortURL": small_url,
     }
 
-    # we need to turn the long_url into a shortened url
-    # and then add it to a global dictionary for right now (database will be added later)
     return jsonify(output), 200
-    
-
-
 
 @app.route('/static/<path:filename>')
 def serve_static(filename):
     return send_from_directory('static', filename)
 
-if __name__ == '__main__':
+FLASK_APP = "Short_URL.main"
+
+#print(__name__) 
+if __name__ == FLASK_APP:
+
+    with app.app_context():
+        db.create_all()
+        print("Database tables recreated")
+
     app.run(debug=True)
